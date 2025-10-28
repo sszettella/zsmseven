6af242c4ff0eb117b6245f98711c25e571fe549b
sszettella <steve@szettella.com>
2025-10-28 08:58:41 -0700

    remove some remanants of old site gen

--- update_headers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to regenerate static blog files with new header
-cd /Users/sszettella/Documents/GitHub/zsmseven
-python publish_pipeline.py &
-sleep 2
-pkill -f publish_pipeline.py
-echo "Static files regenerated"
